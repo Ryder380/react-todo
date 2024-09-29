83e84c50c939230ed201ae5223fd202a25711cc5
Ryder380 <95279419+Ryder380@users.noreply.github.com>
2024-09-29 13:03:57 +0530

    Update footer.jsx

--- src/AppLayout/footer.jsx
+++ src/AppLayout/footer.jsx
@@ -5,7 +5,7 @@ function footer() {
     <div>
       <div className="container border-top py-3 d-flex justify-content-center">
       <p className='m-0'>Author: Kalindu Gayanjith
-      <a className='ps-3' href="mailto:hege@example.com">kalindugayanjith@gmail.com</a></p>
+      <a className='ps-3'>kalindugayanjith@gmail.com</a></p>
       </div>
     </div>
   )
